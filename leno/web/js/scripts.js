2291014f0aa9ac656596023fffc5cf0081f7f5d0
Rida Dahhane <the.dr.rida@gmail.com>
2022-11-25 16:55:47 +0000

    testimonials fix

--- leno/web/js/scripts.js
+++ leno/web/js/scripts.js
@@ -121,6 +121,30 @@ if (checkReplace !== null) {
 
 /* Card Slider - Swiper */
 var cardSlider = new Swiper('.card-slider', {
+	/*autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},*/
+	loop: false,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 3,
+	spaceBetween: 70,
+	breakpoints: {
+		// when window is <= 767px
+		767: {
+			slidesPerView: 1
+		},
+		// when window is <= 991px
+		991: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+var cardSlider = new Swiper('.card-sliderr', {
 	autoplay: {
 		delay: 4000,
 		disableOnInteraction: false
